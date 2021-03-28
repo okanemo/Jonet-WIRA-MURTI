3657f20219e1fe1c35fbc0d31a6454db921df3a7
jonetmurti <13518083@std.stei.itb.ac.id>
2021-03-28 08:52:33 +0700

    implement patientlab model

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class PatientLab(models.Model):
+    id_number = models.CharField(max_length=200, unique=True)
+    patient_name = models.CharField(max_length=200)
+    phone_mobile = models.CharField(max_length=200, blank=True)
+    gender = models.CharField(max_length=200)
+    date_of_birth = models.CharField(max_length=200)
+    date_of_test = models.CharField(max_length=200)
+    lab_number = models.CharField(max_length=200)
+    clinic_code = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+    unit = models.CharField(max_length=200)
+    ref_range = models.CharField(max_length=200)
+    finding = models.CharField(max_length=200)
+    result_state = models.CharField(max_length=200)
--- patient_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PatientAppConfig(AppConfig):
-    name = 'patient_app'
